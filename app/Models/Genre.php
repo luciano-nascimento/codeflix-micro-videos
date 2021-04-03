70f351bffb9c1492133340b03193441234c092e2
Luciano Silva Nascimento <luciano.silva@engie.com>
2021-04-03 20:05:37 -0300

    add Genre resource

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -5,16 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
+class Genre extends Model
 {
     use SoftDeletes, Traits\Uuid;
     protected $fillable = [
         'name',
+        'description',
         'is_active'
     ];
     protected $dates = ['deleted_at'];
     protected $casts = [
         'id' => 'string'
     ];
-
 }
